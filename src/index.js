94cc2ec704a09ba9ebe93d5aef5e41ccae76326b
lsaa <lsaadev.personal@gmail.com>
2025-05-26 21:54:38 -0300

    Inicializar sistema de versionamento do sqlite

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
+import express from 'express';
+
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+import { migrate, db } from './db/database.js';
+migrate();
 
 // Static files
 app.use(express.static('public'))
