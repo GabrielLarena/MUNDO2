e2a4413a6bfc8dbcc61a9adede8b57f6fa963611
lsaa <lsaadev.personal@gmail.com>
2025-05-26 21:33:32 -0300

    Mover img para dentro de public

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'))
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+  console.log(`Listening on port: ${PORT}`)
 })
